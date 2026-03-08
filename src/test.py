c32021f31c3d83649a6a0edcdbaa086d06401f80
thortuwy <tuwy@tuwy.win>
2026-03-08 16:30:25 +0100

    fix: aaaaaaaaaaaaaaaaaaaaaaaaaaa

--- src/test.py
+++
@@ -1,8 +0,0 @@
-from Codeforces.CodeforcesRequester import CodeforcesRequester
-from utils.program_configs import CodeforcesConfig
-
-cf_req=CodeforcesRequester(CodeforcesConfig())
-
-req=cf_req.get_problem_data_by_id("2200","3966075")
-print(req.status_code)
-print(req.text)
